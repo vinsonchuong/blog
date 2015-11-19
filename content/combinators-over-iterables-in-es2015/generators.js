ecff7f5c990ea6a40ddd1398f41952f5ddfdcb13
Vinson Chuong <vinsonchuong@gmail.com>
2015-11-19 13:04:32 -0800

    Start outlining generator combinator article

--- content/combinators-over-iterables-in-es2015/generators.js
+++ content/combinators-over-iterables-in-es2015/generators.js
@@ -12,48 +12,6 @@ export function* filter(fn, iterable) {
   }
 }
 
-export function* head(iterable) {
-  for (const value of iterable) {
-    yield value;
-    break;
-  }
-}
-
-export function* last(iterable) {
-  let previousValue;
-  for (const value of iterable) {
-    previousValue = value;
-  }
-  yield value;
-}
-
-// export function* tail(iterable) {
-//   let iterator = iterable[Symbol.iterator]();
-//   iterator.next();
-//   yield* iterator;
-// }
-export function* tail(iterable) {
-  let first = true;
-  for (const value of iterable) {
-    if (first) {
-      first = false;
-    } else {
-      yield value;
-    }
-  }
-}
-
-export function* init(iterable) {
-  let previousValue;
-  for (const value of first(iterable)) {
-    previousValue = value;
-  }
-  for (const value of iterable) {
-    yield previousValue
-    previousValue = value;
-  }
-}
-
 export function* reduce(fn, iterable) {
   let accumulator;
   for (const value of first(iterable)) {
