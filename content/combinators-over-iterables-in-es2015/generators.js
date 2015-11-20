f46b90d71df7b04ba7d393fe0f83bc118005801f
Vinson Chuong <vinsonchuong@gmail.com>
2015-11-20 13:24:10 -0800

    Add standard list iteration methods to article

--- content/combinators-over-iterables-in-es2015/generators.js
+++ content/combinators-over-iterables-in-es2015/generators.js
@@ -1,27 +1,3 @@
-export function* map(fn, iterable) {
-  for (const value of iterable) {
-    yield fn(value);
-  }
-}
-
-export function* filter(fn, iterable) {
-  for (const value of iterable) {
-    if (fn(value)) {
-      yield value;
-    }
-  }
-}
-
-export function* reduce(fn, iterable) {
-  let accumulator;
-  for (const value of first(iterable)) {
-    accumulator = value;
-  }
-  for (const value of rest(iterable)) {
-    yield accumulator = fn(accumulator, value);
-  }
-}
-
 export function* all(iterable, fn) {
   yield* reduce(function*(accumulator, value) {
     yield accumulator && fn(value);
